c2bf960a88531544ab08a4f1768208c0ba61cd23
thayna1113 <thaynas1113@gmail.com>
2025-05-05 00:18:43 +0000

    novoprintf

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-  int tabuleiro[10][10] = {0};//tabuleiro matrix
+  int tabuleiro[10][10] = {0};//tabuleiro matrix 10x10
   
 
   // Navio horizontal na linha 3, colunas 2 a 4
@@ -18,6 +14,16 @@ int main() {
   tabuleiro[5][8] = 3;
   tabuleiro[6][8] = 3;
   tabuleiro[7][8] = 3;
+
+    // Navio vertical na linha 1 a 3, coluna 6 a 8
+    tabuleiro[8][1] = 3;
+    tabuleiro[7][2] = 3;
+    tabuleiro[6][3] = 3;
+
+     // Navio vertical na linha 1 a 3, coluna 6 a 8
+     tabuleiro[1][8] = 3;
+     tabuleiro[2][7] = 3;
+     tabuleiro[3][6] = 3;
   
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
@@ -26,13 +32,6 @@ int main() {
     printf("\n"); 
   }
 
-
-
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
